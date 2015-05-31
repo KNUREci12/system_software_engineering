455c7f5efca25750e610547a5e53c9ed5a937d37
tikone <tikhon.tik@gmail.com>
2015-05-31 19:17:48 +0300

    add unity

---
+++ spz_unity/Assets/main.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Windows.Forms;
+
+using System.IO;
+
+public class main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        try
+        {
+            var curveList = AudioRecorderUnity.Restore.RestoreCurveList(@"3d.multifft");
+
+            for( int i = 0; i < curveList.Count; ++i )
+                createGraph( curveList[ i ], i );
+        }
+        catch( System.Exception _exception )
+        {
+            Debug.Log( _exception.Message );
+        }
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void createGraph(
+            ZedGraph.CurveItem _curve
+        ,   System.Single _shiftZ
+    )
+    {
+        int z = 1;
+        int counter = 1;
+
+        for (int i = 0; i < _curve.Points.Count - 1; i += counter)
+        {
+            var baseAmpl = _curve[i].Y;
+
+            counter = 1;
+
+            for (int j = i; j < _curve.Points.Count - 1; ++j)
+                if( baseAmpl == _curve[ j ].Y)
+                    ++counter;
+
+
+            var ampl = System.Convert.ToSingle(baseAmpl * System.Math.Pow(10, 9));
+            var rect = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+            rect.transform.localScale = new Vector3(0.002f * counter, 0.001f * ampl, 0.3f);
+            rect.transform.position = new Vector3( -30f + i * 0.002f, 1 + 0.001f * ampl / 2, _shiftZ * 0.3f);
+
+            ++z;
+
+            var color = _curve.Color;
+            rect.GetComponent<Renderer>().material.color = new Color( color.R, color.G, color.B, color.A );
+        }
+
+        Debug.Log( z.ToString() );
+
+        //for (int i = 0; i < 22100; )
+        //{
+        //    var baseAmpl = AudioRecorderUnity.AmplitudeGetter.getAmplitude( fft, i );
+        //
+        //    int counter = 1;
+        //    while( baseAmpl == AudioRecorderUnity.AmplitudeGetter.getAmplitude(fft, ++i) )
+        //        ++counter;
+        //
+        //    var ampl = System.Convert.ToSingle(baseAmpl * System.Math.Pow(10, 8) * 5);
+        //    var rect = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //    rect.transform.localScale = new Vector3(0.002f * counter, 0.001f * ampl, 0.3f);
+        //    rect.transform.position = new Vector3(i * 0.002f, 1 + 0.001f * ampl / 2, 0);
+        //
+        //    rect.GetComponent<Renderer>().material.color = Color.yellow;
+        //}
+    }
+
+}
---
+++ spz_unity/Assets/moving.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moving : MonoBehaviour {
+
+
+    public float shift = 0.1f;
+
+    public float minimumX = -60F;
+    public float maximumX = 60F;
+
+    public float minimumY = 1F;
+    public float maximumY = 100F;
+
+    public float minimumZ = -25F;
+    public float maximumZ = 25F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            var quant = this.transform.rotation;
+            this.transform.position += quant * new Vector3(-shift, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            var quant = this.transform.rotation;
+            this.transform.position += quant * new Vector3(0, 0, shift);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            var quant = this.transform.rotation;
+            this.transform.position += quant * new Vector3(shift, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            var quant = this.transform.rotation;
+            this.transform.position += quant * new Vector3(0, 0, -shift);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            shift *= 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (shift > 0.0001)
+                shift /= 2;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            var y = Input.GetAxis(@"Mouse ScrollWheel") * 5;
+
+            var yAfter = y + this.transform.position.y;
+            if (yAfter > minimumY && yAfter < maximumY)
+                this.transform.position += new Vector3(0, y, 0);
+
+        }
+
+        correctionPosition();
+
+	}
+
+    private void correctionPosition()
+    {
+        var corPos = new Vector3();
+
+        var x = this.transform.position.x;
+
+        if( x < minimumX )
+            corPos += new Vector3( minimumX - x, 0f, 0f );
+        else if( x > maximumX )
+            corPos += new Vector3( maximumX - x, 0f, 0f );
+
+        var y = this.transform.position.y;
+
+        if( y < minimumY )
+            corPos += new Vector3( 0f, minimumY - y, 0f);
+        else if( y > maximumY )
+            corPos += new Vector3( 0f, maximumY - y, 0f);
+
+        var z = this.transform.position.z;
+
+        if( z < minimumZ )
+            corPos += new Vector3( 0f, 0f, minimumZ - z );
+        else if( z > maximumZ )
+            corPos += new Vector3( 0f, 0f, maximumZ - z );
+
+        this.transform.position += corPos;
+
+    }
+
+}
